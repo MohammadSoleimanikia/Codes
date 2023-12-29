97a7c263e2a6f3fdc7f00179abd33ba7deaff06e
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2023-12-29 17:09:51 +0330

    add two example of js codes

---
+++ js/1-primitive-types.js
@@ -0,0 +1,21 @@
+// 1-number
+1;
+1.54;
+-1.54;
+//1-1-math
+2+2;
+2/2;
+// power
+2**2;
+//Modulo
+10%4==1;
+// NAN not a number
+0/0==NaN;
+// 2-string
+
+// 3-boolean
+true;
+false;
+// 4-null
+
+// 5-undefined
---
+++ js/2-variables.js
@@ -0,0 +1,14 @@
+let myVar="hello";
+
+// constant
+const myConst="it can't change";
+
+// the old variable keyword
+var newVar=0;
+
+//Incrementing:
+newVar += 3; //numberOfFriends is now 4
+
+//Naming Conventions
+// Use upper camel-cased names:
+let numberOfChickens = 6;
