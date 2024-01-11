d9681f03d157ecc576af7fdbe62b8b26a1689561
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-01-11 11:04:53 +0330

    complete arrow function example

--- js/11-arrow-function.js
+++ js/11-arrow-function.js
@@ -9,13 +9,34 @@ const sum = (x, y) => {
 }
 
 //if we have one argument we can ignore parenthesis
-const square=x=>{return x*x}
+const square = x => { return x * x }
 
 // implicit return:we can remove return and wrap returned value in parenthesis
 // !!only one line of code should be in the function
-const cube=x=>(x*x*x)
+const cube = x => (x * x * x)
 console.log(cube(2))
 
 // we can also remove parenthesis if write all codes in one line
-const pow=x=>x*x
+const pow = x => x * x
 console.log(pow(4));
+
+// this keyword in arrow vs normal
+
+// normal function
+const person = {
+    firstName: "mohammad",
+    lastName:"soleimanikia",
+    fullName:function(){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+console.log(person.fullName());
+//arrow
+const person1 = {
+    firstName: "mohammad",
+    lastName:"soleimanikia",
+    fullName:()=>{
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+console.log(person1.fullName());//returns undefined because 'this' in arrow function refers to window
