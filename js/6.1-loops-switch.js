97c0c67d625a201aaf807827ccac079b54b78b29
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-07-01 17:40:43 +0330

    add switch example

---
+++ js/6.1-loops-switch.js
@@ -0,0 +1,10 @@
+let weather="rainy";
+switch(weather){
+    case 'rainy':
+        console.log("bring umbrella");
+        // remember to add break
+        break;
+    case 'sunny':
+        console.log("bring sunglasses");
+        break;
+}
