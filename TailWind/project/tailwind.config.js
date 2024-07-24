88bba4a8b6996f7cd207ac8fc77c0580c0622ad7
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-07-24 12:07:36 +0330

    add tailwind.config add add customized properties to it

--- TailWind/project/tailwind.config.js
+++ TailWind/project/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        // adding our own colors 
+        primary: '#ff6363',
+        secondary:{
+          // lighter shade of the primary
+          100:'#e2e2d5',
+          // darker shade of the primary
+          200:'#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
+
