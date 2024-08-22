300a76e8d0cf04a95f68ae5673eab7371a0bcea8
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-22 09:50:06 +0330

    update variables codes

--- js/2-variables.js
+++ js/2-variables.js
@@ -1,10 +1,12 @@
-let myVar="hello";
+// block scoped variables (let and const)
+let myVar = "hello";
 
 // constant
-const myConst="it can't change";
+const myConst = "it can't change";
 
-// the old variable keyword
-var newVar=0;
+// the old variable keyword (function scoped variables )
+var newVar = 0;
+// only available in the parent function
 
 //Incrementing:
 newVar += 3; //numberOfFriends is now 4
@@ -14,4 +16,17 @@ newVar += 3; //numberOfFriends is now 4
 let numberOfChickens = 6;
 
 // define multiple vars in one line
-let one =1, two=2;
+let one = 1,
+  two = 2;
+
+// we can use vars without declaring them
+notDefined = 12;
+// for preventing this use 'use strict'; at the top of the document
+
+// naming
+// camel case
+const iLovePizza = true;
+// UpperCamelCase
+const ILoveToEatHotDogs = false;
+// snake case
+const this_is_snake_case = "cool";
