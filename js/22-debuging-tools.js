487992d1cc3feac5b30afd39d543c7416b96214a
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-25 20:43:53 +0330

    complete debuging tools code

--- js/22-debuging-tools.js
+++ js/22-debuging-tools.js
@@ -26,3 +26,35 @@ function hello(firstName){
 hello('mohammad');
 hello('mohammad');
 hello('jack');
+
+// console.group and console.groupEnd used to group number of consoles
+function doStuff(){
+    console.group("Doing some stuff");
+    console.log("in group 1");
+    console.log("in group 2");
+    console.log("in group 3");
+    console.groupEnd("Doing some stuff");
+}
+doStuff();
+console.log('out of group')
+// use group for people
+people.forEach((person, index) => {
+    console.group(`${person.firstName}`);
+    console.log(person.cool);
+    console.log(person.age);
+    console.groupEnd(`${person.name}`);
+  });   
+
+//2- grabbing element in web
+// if we click inspect on the one element in chrome in console we can access that by typing $0
+// tag value
+// ! its works only in console in web
+$0.value
+
+// next element
+$1
+
+// 3-breakpoints
+// !its works only in web if dev tools are open
+// add debugger; in your code when code reached to this pause from running
+ 
