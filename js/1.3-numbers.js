ecf36adf13d205a4b19d0078d8f70b22456a6ea0
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-09-04 22:29:01 +0330

    update number

--- js/1.3-numbers.js
+++ js/1.3-numbers.js
@@ -4,7 +4,9 @@ console.log(Math.floor(20.9));
 console.log(Math.ceil(70.2));
 // random between 0.0 to 0.9
 console.log(Math.random());
-
+// if we want a random number between two number we use this 
+let randNum=Math.floor(Math.random()*6)// between 0 and 6 (bellow the 6) add one to include six
+console.log('random number is '+randNum);
 // modulo operator 
 console.log(12%5);
 
