e4cf6f457a0b63ac564e59d483eb223e6e03bbcc
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-09-15 22:11:39 +0330

    make server with Express

--- BackEnd/Express.js/01-make-Express-server/index.js
+++ BackEnd/Express.js/01-make-Express-server/index.js
@@ -1,1 +1,19 @@
 // 1- initialize NPM
+// npm init 
+// 2- install express 
+// npm i express 
+// 3- change the type to module for Ecma script modules 
+// "type":"module"
+
+// 4-write the server codes 
+import express from "express";
+const app = express();
+const port =3000;
+// 3000 is port and this is location of our server that we are listening for request from client
+// callback triggered when the server is set up 
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
+
+// start the server : node index.js
+// go to browser : localhost:3000
