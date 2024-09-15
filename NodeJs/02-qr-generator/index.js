e4914b5c14835875955bef4aa034457679d9f370
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-09-15 09:11:14 +0330

    add text to QR logic

--- NodeJs/02-qr-generator/index.js
+++ NodeJs/02-qr-generator/index.js
@@ -1,16 +1,23 @@
 // npm i inquirer qr-image
-// set type to module for Ecma script module 
-import inquirer from 'inquirer';
+// set type to module for Ecma script module
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
 
 inquirer
   .prompt([
     /* Pass your questions in here */
-    // question is an object message : The question to print.
-    // name: 
-    {"message":"please give your URL"}
+    // question is an object. message : The question to print.
+    // name: to use when storing answer
+    { message: "please give your URL",
+       name: "URL" },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    // Use user feedback for... whatever!!    
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+    // write it in a file 
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
