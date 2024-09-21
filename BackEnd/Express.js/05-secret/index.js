0dd39c4ee3a9cdd66e9fbe15e12f1e6e8da8393d
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-09-21 20:23:48 +0330

    add secret project

---
+++ BackEnd/Express.js/05-secret/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// full path extraction
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// initialize app object
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// handling form with post request
+app.post("/check", (req, res) => {
+    // if password was ok then return the secret page 
+  if (req.body["password"] == "123") {
+    res.sendFile(__dirname + "/secret.html");
+  } else {
+    res.send("<h1>wrong pass</h1>");
+  }
+});
+// handling homepage
+app.get("/", (req, res) => {
+  // send home page
+  res.sendFile(__dirname + "/index.html");
+});
+
+// run app on port 3000
+app.listen(port, () => {
+  console.log("app is running on port 3000");
+});
--- BackEnd/Express.js/readme.md
+++ BackEnd/Express.js/readme.md
@@ -13,4 +13,8 @@
 ### 04- middleware
 - body parser:using body parser as middleware to parse the body of request before handling the request with express 
 - Morgan:used to log the requests that comes in your website
--create-middleware: in this example we implement or own middleware and mount it 
+- create-middleware: in this example we implement or own middleware and mount it 
+- handling submit: takes the data from the index.html file and then use body parser to extract data and then return that into html element
+
+### 04- secret
+- a project that check the password and then if the pass was ok it returns the secret page
