32cd4a3ad0c941293e0a0c4074cf0f0787edf4d2
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-10-12 09:12:52 +0330

    init the project

--- BackEnd/EmbededJavaScript/10-REST-APIs/index.js
+++ BackEnd/EmbededJavaScript/10-REST-APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "dd334f5a-9dd3-4a30-9455-3a54ebffe1d0";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
@@ -35,6 +35,13 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  const searchId = req.body.id;
+  try {
+    const result = await axios.post(API_URL + "/secrets/",{});
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
