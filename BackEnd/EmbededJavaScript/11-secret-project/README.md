a6e808e9fa786935d75942a908439702f4601946
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-10-14 09:56:49 +0330

    adding the secret project

---
+++ BackEnd/EmbededJavaScript/11-secret-project/README.md
@@ -0,0 +1,33 @@
+
+# Lisper app
+
+
+
+
+
+## Screenshots
+
+![App Screenshot](./public/images/Screenshot.png)
+
+
+## Run Locally
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  node index.js
+```
+
+
+## Lessons Learned
+
+in this project i review the usage of the Axios and the express and using the API 
+
+https://secrets-api.appbrewery.com/
+
--- BackEnd/EmbededJavaScript/11-secret-project/index.js
+++ BackEnd/EmbededJavaScript/11-secret-project/index.js
@@ -1,13 +1,22 @@
-// HINTS:
 // 1. Import express and axios
-
+import express, { response } from "express";
+import axios from "axios";
 // 2. Create an express app and set the port number.
-
+const app = express();
+const port = 3000;
 // 3. Use the public folder for static files.
-
+app.use(express.static("public"));
 // 4. When the user goes to the home page it should render the index.ejs file.
-
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
+app.get("/", async (req, res) => {
+  // 5. Use axios to get a random secret and pass it to index.ejs to display the
+  // secret and the username of the secret.
+  const Response = await axios.get("https://secrets-api.appbrewery.com/random");
+  let secretAPI = Response.data.secret;
+  let userAPI = Response.data.username;
+  res.render("index.ejs", { secret: secretAPI, user: userAPI });
+});
 
 // 6. Listen on your predefined port and start the server.
+app.listen(port, () => {
+  console.log(`app is started on port ${port}`);
+});
