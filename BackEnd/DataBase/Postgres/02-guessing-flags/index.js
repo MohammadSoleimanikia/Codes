1579e3bce28052e67382e00f0511a28154293486
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-10-20 12:30:10 +0330

    add guessing country name by flag using postgreSQL DB

---
+++ BackEnd/DataBase/Postgres/02-guessing-flags/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg"
+
+const app = express();
+const port = 3000;
+
+const db=new pg.Client(
+  {
+      user:'postgres',
+      host:'localhost',
+      database:'world',
+      password:'12345',
+      port:5432,
+  }
+  );
+  db.connect();
+let quiz=[];
+// using values from DB.
+db.query("SELECT * FROM flags", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+
+
+// GET home page
+app.get("/", (req, res) => {
+  totalCorrect = 0;
+  nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ BackEnd/DataBase/Postgres/02-guessing-flags/readme.md
@@ -0,0 +1,23 @@
+# Flag Quiz App
+
+A fun web application where users guess the country name based on the displayed flag emoji. Built with Node.js, Express, and PostgreSQL.
+
+## Features
+
+- Randomly displays flag emojis.
+- Users can submit their answers.
+- Tracks and displays the total score.
+- Responsive design.
+
+## Technologies Used
+
+- **Node.js**: JavaScript runtime for server-side development.
+- **Express**: Web framework for building the application.
+- **PostgreSQL**: Database for storing flag data.
+- **EJS**: Templating engine for rendering HTML views.
+
+## warning :
+- flags shows as character in chrome it works in firefox only 
+- to run on the chrome install this extension :
+
+[country Flag Fixer](https://chromewebstore.google.com/detail/country-flag-fixer/jhcpefjbhmbkgjgipkhndplfbhdecijh)
