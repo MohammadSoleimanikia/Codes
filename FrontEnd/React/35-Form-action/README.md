9236ea5663a908edfa7edd0b6e7c7656055c6694
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2025-03-25 20:36:45 +0330

    change the input to keep values if we have error

---
+++ FrontEnd/React/35-Form-action/README.md
@@ -0,0 +1,146 @@
+# Form action in react
+
+this feature build into react 19 and is a alternative to handling form submission manually
+
+## what are Form actions
+
+- available in react 19+
+
+## usage :
+
+1. define the function
+
+```jsx
+// no need to pass event instead pass formData
+// preventDefault is called by react no need to use it
+function signupAction(formData) {
+  // the argument should be equal to input name
+  const enteredEmail = formData.get("email");
+  console.log(enteredEmail);
+}
+```
+
+2. use action prop in form element
+
+```jsx
+<form action={signupAction}>
+  <div className="control">
+    <label htmlFor="email">Email</label>
+    <input id="email" type="email" name="email" />
+  </div>
+</form>
+```
+
+## adding validation checks
+
+```jsx
+function signupAction(formData) {
+  const email = formData.get("email");
+  // for multiple checkbox use getAll
+  const acquisitionChannel = formData.getAll("acquisition");
+  // other inputs ...
+
+  let error = [];
+  if (!isEmail(email)) {
+    error.push("Invalid email address");
+  }
+  if (acquisitionChannel.length === 0) {
+    error.push("please select at least one acquisition channel.");
+  }
+  // if we have error pass error
+  if (error.length > 0) {
+    return { errors };
+  }
+  // // form reset after submission 
+  return { errors: null };
+}
+```
+
+## useActionState : managing form-dependent state
+
+in the previous section we produce error by return it as object but to use it we need to this hook
+react 19+
+
+### usage
+
+1. import
+
+```jsx
+import { useActionState } from "react";
+```
+
+2. call after the action function
+
+- first argument is action function
+- second argument should be the initial value because until the form did not submitted we don't have the value
+  it return three element
+- current form state in our example first time is null then maybe errors added to this
+- updated form action : its our form action with extra features and this is should added as value to form `action` prop
+- pending element: which is true or false depending on whether the form is currently submitted or not (optional)
+
+```jsx
+// if we use form action in react we have to add prevFormState as first argument and formData as second argument
+function signupAction(prevFormState, formData) {}
+
+const [formState, formAction] = useActionState(signupAction, { errors: null });
+
+<form action={formAction}></form>;
+```
+
+### improvement
+
+now all of the values after submission are removed its better that if we have error the previous values stay in input fields
+
+we add enteredValue property to the returned state object
+
+```jsx
+return {
+  errors,
+  enteredValues: {
+    email,
+    password,
+    confirmPassword,
+    firstName,
+    lastName,
+    role,
+    acquisitionChannel,
+    terms,
+  },
+};
+```
+
+use this object into input section in `defaultValue` props
+
+```jsx
+// use question mark to ignore when entered value is undefined
+<input id="email" type="email" name="email" defaultValue={formState.enteredValues?.email} />
+// for select we add defaultValue to select not the options
+<select id="role" name="role" defaultValue={formState.enteredValues?.role}>
+    <option value="student">Student</option>
+    <option value="teacher">Teacher</option>
+</select>
+
+// for the checkbox with equal name we use defaultCheck instead of default 
+<div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel}
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel}
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+```
+
--- FrontEnd/React/35-Form-action/src/components/Signup.jsx
+++ FrontEnd/React/35-Form-action/src/components/Signup.jsx
@@ -1,18 +1,88 @@
+import {
+  isEmail,
+  isNotEmpty,
+  isEqualToOtherValue,
+  hasMinLength,
+} from "../util/validation";
+import { useActionState } from "react";
 export default function Signup() {
+  function signupAction(prevFormState, formData) {
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirm-password");
+    const firstName = formData.get("first-name");
+    const lastName = formData.get("last-name");
+    const role = formData.get("role");
+    const terms = formData.get("terms");
+    // for multiple checkbox use getAll
+    const acquisitionChannel = formData.getAll("acquisition");
+
+    let errors = [];
+    if (!isEmail(email)) {
+      errors.push("Invalid email address");
+    }
+    if (!isNotEmpty(password) || hasMinLength(password, 6)) {
+      errors.push("You must provide a password with at least six character");
+    }
+    if (isEqualToOtherValue(password, confirmPassword)) {
+      errors.push("passwords do not match.");
+    }
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("please provide both your first and last name");
+    }
+    if (!terms) {
+      errors.push("You must agree to the terms and conditions.");
+    }
+    if (acquisitionChannel.length === 0) {
+      errors.push("please select at least one acquisition channel.");
+    }
+    // if we have error pass error
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisitionChannel,
+          terms,
+        },
+      };
+    }
+    // form reset after submission 
+    return { errors: null };
+  }
+
+  const [formState, formAction] = useActionState(signupAction, {
+    errors: null,
+  });
   return (
-    <form>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email}
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password}
+          />
         </div>
 
         <div className="control">
@@ -21,6 +91,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword}
           />
         </div>
       </div>
@@ -30,18 +101,32 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role}
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -58,6 +143,7 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('google')}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -68,23 +154,37 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('friend')}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultChecked={formState.enteredValues?.acquisitionChannel.includes('other')}
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" defaultValue={formState.enteredValues?.terms}/>I
           agree to the terms and conditions
         </label>
       </div>
 
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <p className="form-actions">
         <button type="reset" className="button button-flat">
           Reset
