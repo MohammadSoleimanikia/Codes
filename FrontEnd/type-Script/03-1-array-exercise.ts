1c00f5ab3d3f4f45f22ed43257d9e264d787699a
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2025-08-10 16:23:15 +0330

    array example added

--- FrontEnd/type-Script/03-1-array-exercise.ts
+++ FrontEnd/type-Script/03-1-array-exercise.ts
@@ -2,22 +2,32 @@
 // ******************* PART 1 *******************
 // **********************************************
 // Create an empty array of numbers called "ages":
-
+let ages: number[] = [];
 // **********************************************
 // ******************* PART 2 *******************
 // **********************************************
 // Create an array variable called gameBoard that starts as an empty array.
 // It should be typed to hold a 2 dimensional array of strings
-
+let gameBoard: number[][] = [[]];
 // **********************************************
 // ******************* PART 3 *******************
 // **********************************************
 // Create a Product type that contains a name and a price.
 // An example product could be:
 // {name: "coffee mug", price: 11.50}
+type Product = { name: string; price: number };
 
+let products: Product[] = [];
+products.push({ name: "coffee mug", price: 11.5 });
 // **********************************************
 // ******************* PART 4 *******************
 // **********************************************
 // Write a function called getTotal that accepts an array of Product types
 // It should return the sum of all the products' prices
+function getTotal(products:Product[]):number{
+    let sum=0
+    products.forEach(product => {
+        sum+=product.price;
+    });
+    return sum;
+}
