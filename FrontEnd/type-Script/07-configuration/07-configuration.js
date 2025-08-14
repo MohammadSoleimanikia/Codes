4c931ef736a2fb160c56bcbb5e0dc0eb5840d2d1
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2025-08-14 07:55:26 +0330

    add ts.config example

---
+++ FrontEnd/type-Script/07-configuration/07-configuration.js
@@ -0,0 +1,6 @@
+var norma = {
+    breed: 'silkie',
+    eggsPerWeek: 4,
+    name: 'norma',
+    age: 2
+};
---
+++ FrontEnd/type-Script/07-configuration/07-configuration.ts
@@ -0,0 +1,12 @@
+interface Chicken{
+    breed:string;
+    eggsPerWeek:number;
+    name:string;
+    age:number;
+}
+const norma:Chicken={
+    breed:'silkie',
+    eggsPerWeek:4,
+    name:'norma',
+    age:2
+}
